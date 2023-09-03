ee0f0aed244bef9ff79385fe605a2901a84feb3b
DearShervin <sherviniranaghideh@gmail.com>
2023-09-04 00:04:50 +0330

    Updating the app, making it like a chat with Cohere AI

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cohere
 import customtkinter as ctk
+import threading
 
-co = cohere.Client("Your API Key")
+co = cohere.Client("APIKEY")
 
 class App():
     def __init__(self):
@@ -20,26 +21,39 @@ class App():
         self.text_field.grid(row=1, column=0, pady=5, padx=5, sticky="NWES")
 
         self.text_button = ctk.CTkButton(
-            master=self.mainframe, text="Enter", command=self.Resp)
+            master=self.mainframe, text="Enter", command=self.on_click)
         self.text_button.grid(row=1, pady=5, column=1, sticky="NWES")
 
         self.text_1 = ctk.CTkTextbox(
             self.root, pady=10, padx=50, width=780, height=280)
         self.text_1.pack()
 
+        self.root.bind('<Return>', self.on_click)
+
         self.root.mainloop()
         return
 
-    def Resp(self):
-        self.prompt = self.text_field.get()
+    def on_click(self, event=None):
+        user_input = self.text_field.get()
+        self.display_message(f'You: {user_input}')
+        self.thread = threading.Thread(target=self.resp, args=(user_input,))
+        self.thread.start()
+
+    def display_message(self, message):
+        formatted_message = f'{message}\n\n'
+        self.text_1.insert('end', formatted_message)
+
+    def resp(self, user_input):
         self.response = co.generate(
             model='command-xlarge-nightly',
-            prompt=self.prompt,
+            prompt=user_input,
             max_tokens=300,
             temperature=0.6,
             stop_sequences=["--"])
-        self.answer = self.response.generations[0].text
-        self.text_1.insert('end', text=self.answer)
+        api_output = f'Cohere: {self.response.generations[0].text}'
+        self.display_message(api_output)
+        self.text_field.delete(0, 'end')
+
 
 if __name__ == "__main__":
     App()
